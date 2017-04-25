60dc80b8a630b8ea8691ac691b7f2e4ce708d1a9
Yann ILAS <yann.ilas@gmail.com>
2017-04-25 17:02:19 +0200

    initial revision

---
+++ device_check_timeout.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# vim: :set ts=8 et sw=4 sts=4
+
+hostname=`hostname`
+
+find /sys/class/scsi_generic/*/device/timeout -exec grep -H . '{}' \; | tr ':' ' ' |\
+    while read dev timeout; do
+        if [[ $timeout -ge 180 ]]; then
+            echo "  $hostname : INFO : device $dev ; timeout = $timeout"
+        else
+            echo "  $hostname : ERROR : device $dev ; timeout = $timeout"
+        fi
+    done
+
+exit 0
