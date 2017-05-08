1204b9b2b391e895bdea56957330b3bcb2b75857
Yann ILAS <yann.ilas@gmail.com>
2017-05-08 17:50:59 +0200

    Update device_check_timeout.sh

--- device_check_timeout.sh
+++ device_check_timeout.sh
@@ -7,9 +7,9 @@ hostname=`hostname`
 find /sys/class/scsi_generic/*/device/timeout -exec grep -H . '{}' \; | tr ':' ' ' |\
     while read dev timeout; do
         if [[ $timeout -ge 180 ]]; then
-            echo "  $hostname : INFO : device $dev ; timeout = $timeout"
+            echo "  $hostname ; INFO ; device ; $dev ; timeout ; $timeout"
         else
-            echo "  $hostname : ERROR : device $dev ; timeout = $timeout"
+            echo "  $hostname ; ERROR ; device ; $dev ; timeout ; $timeout"
         fi
     done
 
